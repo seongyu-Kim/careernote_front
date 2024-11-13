f8a4ec84316c2799237bbbc60d399484cced7fae
[React1] 김선규 <seongyu1066@naver.com>
2024-11-13 17:49:58 +0900

    ✨ feat: 네비게이션바 및 사이드바 생성

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import * as Styled from './Navbar.styled';
+
+// 네비게이션바 컴포넌트
+const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState<string>('');
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const handleMenuItemClick = (menuName: string) => {
+    setSelectedMenu(menuName);
+  };
+
+  return (
+    <>
+      {/* 네비게이션바 */}
+      <Styled.Nav>
+        <Styled.MenuButton onClick={toggleMenu}>☰</Styled.MenuButton>
+
+        <Styled.Logo>CareerNote</Styled.Logo>
+
+        <Styled.UserInfo>
+          <Styled.Button>김선규 님</Styled.Button>
+          <Styled.Button>Logout</Styled.Button>
+        </Styled.UserInfo>
+      </Styled.Nav>
+
+      {/* 사이드바 */}
+      {isMenuOpen && (
+        <Styled.Menu>
+          <Styled.MenuItem>
+            <Styled.MenuItemBtn
+              isSelected={selectedMenu === '자유게시판'}
+              onClick={() => handleMenuItemClick('자유게시판')}>
+              자유게시판
+            </Styled.MenuItemBtn>
+          </Styled.MenuItem>
+          <Styled.MenuItem>
+            <Styled.MenuItemBtn
+              isSelected={selectedMenu === '공지'}
+              onClick={() => handleMenuItemClick('공지')}>
+              공지
+            </Styled.MenuItemBtn>
+          </Styled.MenuItem>
+          <Styled.MenuItem>
+            <Styled.MenuItemBtn
+              isSelected={selectedMenu === '등업'}
+              onClick={() => handleMenuItemClick('등업')}>
+              등업
+            </Styled.MenuItemBtn>
+          </Styled.MenuItem>
+          <Styled.MenuItem>
+            <Styled.MenuItemBtn
+              isSelected={selectedMenu === '취업'}
+              onClick={() => handleMenuItemClick('취업')}>
+              취업
+            </Styled.MenuItemBtn>
+          </Styled.MenuItem>
+          <Styled.MenuItem>
+            <Styled.MenuItemBtn
+              isSelected={selectedMenu === '스터디'}
+              onClick={() => handleMenuItemClick('스터디')}>
+              스터디
+            </Styled.MenuItemBtn>
+          </Styled.MenuItem>
+        </Styled.Menu>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
