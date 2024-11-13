27b0e0eac93bb80975868ae542ddf951656511a9
[React1] 김선규 <seongyu1066@naver.com>
2024-11-13 12:02:01 +0900

    :tada: chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalStyles from '@styles/Globalstyles';
+
+function App() {
+  return <div>App.tsx</div>;
+  //   <Router>
+  //   <GlobalStyles />
+  //   <Routes>
+  //     <Route path="/" element={<Main />} />
+  //     <Route path="/list" element={<List />} />
+  //   </Routes>
+  // </Router>
+}
+
+export default App;
