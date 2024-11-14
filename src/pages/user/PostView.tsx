a8136aa294c58d3b72e422120a2784d340e917ef
[React1] 이주영 <gmltns5723@kakao.com>
2024-11-14 16:34:21 +0900

    ✨ 게시글 작성 레이아웃 작업 및 MainLayout 적용
    게시글 작성 레이아웃 작업 및 MainLayout 적용

--- src/pages/user/PostView.tsx
+++ src/pages/user/PostView.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@components/Navbar/Navbar';
+import MainLayout from '@components/MainLayout/MainLayout';
 import PostCard from '@components/PostCard/PostCard';
 import { useAlertStore } from '@stores/store';
 import React from 'react';
@@ -9,19 +9,20 @@ const PostView: React.FC = () => {
   const { isOpen, message, openModal, closeModal } = useAlertStore();
   return (
     <>
-      <Navbar />
-      <PostCard
-        category="취업"
-        title="제목입니다~"
-        writer="리오니"
-        date="2024.11.03"
-        content="content 입니다. content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다"
-        user={user} //user가 writer랑 일치해야만 수정, 삭제 버튼 떠야함
-        isOpen={isOpen}  // 모달의 열림 여부 전달
-        message={message}  // 모달에 표시할 메시지 전달
-        openModal={openModal}  // 모달 열기 함수 전달
-        closeModal={closeModal}  // 모달 닫기 함수 전달
-      />
+      <MainLayout>
+        <PostCard
+          category="취업"
+          title="제목입니다~"
+          writer="리오니"
+          date="2024.11.03"
+          content="content 입니다. content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다 content 입니다"
+          user={user} //user가 writer랑 일치해야만 수정, 삭제 버튼 떠야함
+          isOpen={isOpen}  // 모달의 열림 여부 전달
+          message={message}  // 모달에 표시할 메시지 전달
+          openModal={openModal}  // 모달 열기 함수 전달
+          closeModal={closeModal}  // 모달 닫기 함수 전달
+        />
+      </MainLayout>
     </>
   );
 };
