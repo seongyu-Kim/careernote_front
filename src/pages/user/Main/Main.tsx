7b1834992a43902c4acf00fbef0c874a48e6ae11
[React1] 김선규 <seongyu1066@naver.com>
2024-11-21 03:35:03 +0900

    ✨ feat: 카테고리 별 게시글 필터링 기능

--- src/pages/user/Main/Main.tsx
+++ src/pages/user/Main/Main.tsx
@@ -1,377 +1,34 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import useCategoryStore from '@stores/useCategoryStore';
+import { usePostStore } from 'stores/usePostStore';
 import { useLocation } from 'react-router-dom';
 import { NavbarContainer, PostList, Pagination } from 'components';
 import Banner from '@assets/Banner.png';
 import * as Styled from './Main.styled';
-import apiUtils from '@utils/apiUtils';
-import { BOARD_API, NOTICE_API } from '@routes/apiRoutes';
-
-const { ALL_NOTICES } = NOTICE_API;
-const { ALL_BOARD } = BOARD_API;
-
-// const dummyPosts: Post[] = [];
-
-// const dummyPosts: Post[] = [
-//   {
-//     _id: 1,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 2,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 3,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 4,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 5,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 6,
-//     category: '자유게시판',
-//     title: '여섯',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 7,
-//     category: '공지',
-//     title: '일곱',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 8,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 9,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 10,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 11,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 12,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 13,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 14,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 15,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 16,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 17,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 18,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 19,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 20,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 21,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 22,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 23,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 24,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 25,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 26,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 27,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 28,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 29,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 30,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 31,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 32,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 33,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 34,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 35,
-//     category: '자유게시판',
-//     title: '다섯 번째 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 36,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 37,
-//     category: '등업',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 38,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 39,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 40,
-//     category: '자유게시판',
-//     title: '끝!!!!!!!! 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-//   {
-//     _id: 41,
-//     category: '자유게시판',
-//     title: '첫 번째 게시글',
-//     user: '김선규',
-//     createdAt: '2024-11-13',
-//   },
-//   {
-//     _id: 42,
-//     category: '공지',
-//     title: '두 번째 게시글',
-//     user: '박주호',
-//     createdAt: '2024-11-12',
-//   },
-//   {
-//     _id: 43,
-//     category: '취업',
-//     title: '세 번째 게시글',
-//     user: '이주영',
-//     createdAt: '2024-11-11',
-//   },
-//   {
-//     _id: 44,
-//     category: '스터디',
-//     title: '네 번째 게시글',
-//     user: '조아라',
-//     createdAt: '2024-11-10',
-//   },
-//   {
-//     _id: 45,
-//     category: '자유게시판',
-//     title: '진짜 마지막 게시글',
-//     user: '하정우',
-//     createdAt: '2024-11-09',
-//   },
-// ];
-
-interface Post {
-  _id: number;
-  title: string;
-  category: string;
-  user: User;
-  createdAt: string;
-}
-interface User {
-  _id: number;
-  nickname: string;
-  level: string;
-}
 
 const Main = () => {
   const location = useLocation();
   const isMyPost = location.pathname === '/mypage';
 
+  const { filteredPosts, fetchAllPosts, selectedCategory } = usePostStore();
+
   const savedPage = sessionStorage.getItem('currentPage');
   const [currentPage, setCurrentPage] = useState<number>(savedPage ? parseInt(savedPage, 10) : 1);
-  const [posts, setPosts] = useState<Post[]>([]);
-  // const { selectedCategory } = useCategoryStore();
+
+  //페이지네이션
   const postsPerPage = 20;
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
 
-  // const filteredPosts =
-  //   selectedCategory === '자유게시판'
-  //     ? posts
-  //     : posts.filter((post) => post.category === selectedCategory);
-  // const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
-  // const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
-
-  const currentPosts = (posts || []).slice(indexOfFirstPost, indexOfLastPost);
-  const totalPages = Math.ceil((posts || []).length / postsPerPage);
+  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
     sessionStorage.setItem('currentPage', page.toString());
   };
 
-  //테이블 데이터
+  //테이블
   const columns = [
     { key: 'category', label: '카테고리', flex: '1' },
     { key: 'title', label: '제목', flex: '3' },
@@ -380,42 +37,8 @@ const Main = () => {
   ];
 
   useEffect(() => {
-    const fetchNotices = async () => {
-      try {
-        const response = await apiUtils({
-          url: ALL_NOTICES,
-          method: 'GET',
-        });
-        console.log('공지:', response);
-        return response.Notices;
-      } catch (error) {
-        console.error('공지 가져오기 실패:', error);
-        return [];
-      }
-    };
-
-    const fetchPosts = async () => {
-      try {
-        const response = await apiUtils({
-          url: ALL_BOARD,
-          method: 'GET',
-        });
-        console.log('게시글:', response);
-        return response.boards;
-      } catch (error) {
-        console.error('게시글 가져오기 실패:', error);
-        return [];
-      }
-    };
-
-    const fetchAll = async () => {
-      const [notices, posts] = await Promise.all([fetchNotices(), fetchPosts()]);
-
-      setPosts([...notices, ...posts]);
-    };
-
-    fetchAll();
-  }, []);
+    fetchAllPosts();
+  }, [selectedCategory, fetchAllPosts]);
 
   return (
     <NavbarContainer>
