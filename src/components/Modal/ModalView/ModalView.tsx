42e71a036012adab913baf42f99111b939710276
[React1] 박주호 <parkjh001217@gmail.com>
2024-11-21 17:13:50 +0900

    :recycle: 모달 추가로인한 모달 상태 함수 전달값 변경

--- src/components/Modal/ModalView/ModalView.tsx
+++ src/components/Modal/ModalView/ModalView.tsx
@@ -1,10 +1,14 @@
 import { useModal } from '@stores/store';
 import FindPassword from '@pages/user/FindPassword/FindPassword';
+import { MyInfo } from '@pages/user/MyInfo/MyInfo';
 //모달 상태 따라서 분기 처리
 export default function ModalView() {
   const { modalState } = useModal();
   if (modalState === 'findPassword') {
     return <FindPassword />;
+  }
+  if (modalState === 'MyInfo') {
+    return <MyInfo />;
   }
   return null;
 }
