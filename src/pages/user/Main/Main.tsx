c71ef4e4a01b9982e2592b4eaf9ab8f2e349e986
[React1] 김선규 <seongyu1066@naver.com>
2024-11-22 13:40:11 +0900

    ✨ feat: 페이지네이션 API 연동

--- src/pages/user/Main/Main.tsx
+++ src/pages/user/Main/Main.tsx
@@ -1,34 +1,92 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { usePostStore } from 'stores/usePostStore';
+// import { useLocation } from 'react-router-dom';
+// import { NavbarContainer, PostList, Pagination } from 'components';
+// import Banner from '@assets/Banner.png';
+// import * as Styled from './Main.styled';
+
+// const Main = () => {
+//   const location = useLocation();
+//   const isMyPost = location.pathname === '/mypage';
+//   //usePostStore
+//   const { filteredPosts, fetchAllPosts, selectedCategory, totalPostCount } = usePostStore();
+
+//   const savedPage = sessionStorage.getItem('currentPage');
+//   // const [currentPage, setCurrentPage] = useState<number>(savedPage ? parseInt(savedPage, 10) : 1);
+//   const [currentPage, setCurrentPage] = useState<number>(1);
+
+//   const postsPerPage = 20;
+//   const totalPages = Math.ceil(totalPostCount / postsPerPage);
+
+//   const handlePageChange = async (page: number) => {
+//     setCurrentPage(page);
+//     // sessionStorage.setItem('currentPage', page.toString());
+//     await fetchAllPosts(page, postsPerPage);
+//   };
+
+//   // 테이블 컬럼
+//   const columns = [
+//     { key: 'category', label: '카테고리', flex: '1' },
+//     { key: 'title', label: '제목', flex: '3' },
+//     { key: 'user', label: '작성자', flex: '1' },
+//     { key: 'createdAt', label: '작성일', flex: '1' },
+//   ];
+
+//   // 초기 데이터 로드
+//   useEffect(() => {
+//     fetchAllPosts(currentPage, postsPerPage);
+//   }, [selectedCategory, currentPage, fetchAllPosts]);
+
+//   return (
+//     <NavbarContainer>
+//       <Styled.LogoImg src={Banner} alt="Landing" />
+//       <PostList isMyPost={isMyPost} posts={filteredPosts} columns={columns} />
+//       <Pagination
+//         currentPage={currentPage}
+//         totalPages={totalPages}
+//         onPageChange={handlePageChange}
+//       />
+//     </NavbarContainer>
+//   );
+// };
+
+// export default Main;
+import React, { useEffect, useState } from 'react';
 import { usePostStore } from 'stores/usePostStore';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { NavbarContainer, PostList, Pagination } from 'components';
 import Banner from '@assets/Banner.png';
 import * as Styled from './Main.styled';
 
 const Main = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const isMyPost = location.pathname === '/mypage';
 
-  const { filteredPosts, fetchAllPosts, selectedCategory } = usePostStore();
+  // usePostStore
+  const { filteredPosts, fetchAllPosts, selectedCategory, totalPostCount } = usePostStore();
 
-  const savedPage = sessionStorage.getItem('currentPage');
-  const [currentPage, setCurrentPage] = useState<number>(savedPage ? parseInt(savedPage, 10) : 1);
+  //페이지 번호
+  const queryParams = new URLSearchParams(location.search);
+  const initialPage = parseInt(queryParams.get('page') || '1');
 
-  //페이지네이션
+  const [currentPage, setCurrentPage] = useState<number>(initialPage);
+
   const postsPerPage = 20;
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const totalPages = Math.ceil(totalPostCount / postsPerPage);
 
-  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
-  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+  //
+  useEffect(() => {
+    fetchAllPosts(currentPage, postsPerPage);
+  }, [selectedCategory, currentPage, fetchAllPosts]);
 
-  const handlePageChange = (page: number) => {
+  const handlePageChange = async (page: number) => {
     setCurrentPage(page);
-    sessionStorage.setItem('currentPage', page.toString());
+    navigate(`?page=${page}`);
+    await fetchAllPosts(page, postsPerPage);
   };
 
-  //테이블
+  // 테이블 컬럼
   const columns = [
     { key: 'category', label: '카테고리', flex: '1' },
     { key: 'title', label: '제목', flex: '3' },
@@ -36,14 +94,10 @@ const Main = () => {
     { key: 'createdAt', label: '작성일', flex: '1' },
   ];
 
-  useEffect(() => {
-    fetchAllPosts();
-  }, [selectedCategory, fetchAllPosts]);
-
   return (
     <NavbarContainer>
       <Styled.LogoImg src={Banner} alt="Landing" />
-      <PostList isMyPost={isMyPost} posts={currentPosts} columns={columns} />
+      <PostList isMyPost={isMyPost} posts={filteredPosts} columns={columns} />
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
