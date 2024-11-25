e91acea48fb6b60f887562acc042eff0d560ebce
[React1]하정우 <hjh268100@gmail.com>
2024-11-25 15:38:03 +0900

    ✨ Feat : 에러페이지 제작

--- src/pages/user/Error/ErrorPage.tsx
+++ src/pages/user/Error/ErrorPage.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorPage = () => {
-  return <div style={{ fontSize: '30px', textAlign: 'center', fontWeight: 'bold' }}>404 Error</div>;
+  return (
+    <div style={{ display: "flex", flexDirection :"column",justifyContent: 'center', alignItems: 'center', height: '100vh', }}>
+      <div style={{ fontSize: '30px', fontWeight: "bold" }}>
+        잘못된 경로입니다.
+      </div>
+      <img src = "/404error.png" alt='notFondimg' style ={{margin : "20px", width : "1000px", height : "600px"}}/>
+      <div style={{ fontSize: '25px', fontWeight: "bold", marginTop:"20px" }}>
+        처음으로 돌아가세요
+      </div>
+
+
+    </div >
+  )
 };
 
 export default ErrorPage;
